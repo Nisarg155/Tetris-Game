024ef96f4f0cf66096568e588401bb4758c52e57
Nisarg <amlaninisarg15@gmail.com>
2023-08-14 14:08:44 +0530

    added display for next

--- tetris.js
+++ tetris.js
@@ -1,105 +1,129 @@
 // writing an js file for tetris game 
 
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
     const width = 10;
     const gird = document.querySelector(".grid");
-    let squares  = Array.from(document.querySelectorAll(".grid div"));
+    let squares = Array.from(document.querySelectorAll(".grid div"));
+    let nextSquares = Array.from(document.querySelectorAll(".mini_grid div"));
     const ScoreDisplay = document.getElementById('score')
     const StartBtn = document.getElementById('start')
+    const displaywidth = 4;
+    let displayindex = 0;
 
     // tetraminos 
     const ltmno = [
-        [1,width+1,width*2 + 1 , 2],
-        [width,width+1,width+2,width*2+2],
-        [1,width+1,width*2+1,width*2],
-        [width,width*2,width*2+1,width*2+2]
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
     ]
 
+
+
     const ztmno = [
-        [width*3,width*3+1,width*2+1,width*2+2],
-        [0,width,width+1,width*2+1],
-        [width*3,width*3+1,width*2+1,width*2+2],
-        [0,width,width+1,width*2+1]
+        [width * 3, width * 3 + 1, width * 2 + 1, width * 2 + 2],
+        [0, width, width + 1, width * 2 + 1],
+        [width * 3, width * 3 + 1, width * 2 + 1, width * 2 + 2],
+        [0, width, width + 1, width * 2 + 1]
     ]
 
     const ttmno = [
-        [1,width,width+1,width+2],
-        [1,width+1,width+2,width*2+1],
-        [width,width+1,width+2,width*2+1],
-        [1,width,width+1,width*2+1]
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
     ]
 
-    const stmno =[
-        [0,1,width,width+1],
-        [0,1,width,width+1],
-        [0,1,width,width+1],
-        [0,1,width,width+1]
+    const stmno = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
     ]
 
     const itmno = [
-        [1,width+1,width*2+1,width*3+1],
-        [width,width+1,width+2,width+3],
-        [1,width+1,width*2+1,width*3+1],
-        [width,width+1,width+2,width+3]
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
     ]
 
-    const tetraminos = [ltmno,ztmno,ttmno,stmno,itmno];
-    let tmno ;
-    let rot;
-    function random_pos( )
-    {
-        return parseInt(Math.random()*10%7);
+    
+
+    
+
+    const tetraminos = [ltmno, ztmno, ttmno, stmno, itmno];
+    const nexttmnos = [
+        [1,5,9,10], //ltmno
+        [5,6,10,11], //ztmno
+        [5,6,7,10], //ttmno
+        [5,6,9,10], //stmno
+        [1,5,9,13] //itmno
+    ]
+    let [tmno, rot] = random_rotation();
+    let [nexttmno,nextrot] = random_rotation();
+    let nextrotation = nexttmnos[nexttmno];
+    draw_next();
+    console.log(nextSquares);
+    function random_pos() {
+        return parseInt(Math.random() * 10 % 7);
     }
 
-    function random_rotation()
-    {
-        tmno = parseInt(Math.random()*10%5);
-        rot = parseInt(Math.random()*10%4);
-        return tetraminos[tmno][rot];
+    function random_rotation() {
+        let tmno = parseInt(Math.random() * 10 % 5);
+        let rot = parseInt(Math.random() * 10 % 4);
+        return [tmno, rot];
     }
 
-    let  currentpos = random_pos(); 
-    
-    let rotation = random_rotation();
-    
-    function draw()
-    {
-        rotation.forEach(index =>{
+    let currentpos = random_pos();
+
+    let rotation = tetraminos[tmno][rot];
+
+    function draw() {
+        rotation.forEach(index => {
             squares[currentpos + index].classList.add('tetraminos');
         })
     }
 
-
-    function undraw()
-    {
-        rotation.forEach(index =>{
-            squares[currentpos+index].classList.remove('tetraminos');
+    function draw_next() {
+        nextrotation.forEach(index => {
+            nextSquares[index].classList.add('tetraminos');
         })
     }
-    
-    let timerID = setInterval(moveDown,500); 
-    document.addEventListener('keyup',control)
-    function control(events)
-    {
-        if(events.key === "ArrowLeft")
-        {
+
+    function undraw_next() {
+
+
+        nextrotation.forEach(index => {
+            nextSquares[index].classList.remove('tetraminos');
+        })
+    }
+
+
+    function undraw() {
+        rotation.forEach(index => {
+            squares[currentpos + index].classList.remove('tetraminos');
+        })
+    }
+
+    let timerID = setInterval(moveDown, 500);
+    document.addEventListener('keyup', control)
+    function control(events) {
+        if (events.key === "ArrowLeft") {
             moveLeft();
         }
-        else if(events.key === "ArrowRight")
-        {
+        else if (events.key === "ArrowRight") {
             moveRight();
         }
-        else if(events.key === "ArrowUp")
-        {
+        else if (events.key === "ArrowUp") {
             rotate();
         }
-        else if(events.key === "ArrowDown")
-        {
+        else if (events.key === "ArrowDown") {
             moveDown();
         }
-    
+
     }
-    
+
     function moveDown() {
         undraw();
         const nextPosition = currentpos + width;
@@ -114,7 +138,7 @@ document.addEventListener('DOMContentLoaded',() => {
         undraw();
         const isAtRightEdge = rotation.some(index => (currentpos + index) % width === width - 1);
         if (!isAtRightEdge) {
-            currentpos += 1;  
+            currentpos += 1;
         }
         if (rotation.some(index => squares[currentpos + index].classList.contains('taken'))) {
             currentpos -= 1;
@@ -122,7 +146,7 @@ document.addEventListener('DOMContentLoaded',() => {
         }
         draw();
     }
-    
+
 
 
     function moveLeft() {
@@ -130,51 +154,56 @@ document.addEventListener('DOMContentLoaded',() => {
         const isAtLeftEdge = rotation.some(index => (currentpos + index) % width === 0);
         if (!isAtLeftEdge) {
             currentpos -= 1;
-        }   
+        }
         if (rotation.some(index => squares[currentpos + index].classList.contains('taken'))) {
             currentpos += 1;
         }
         draw();
     }
-
-    function rotate(){
+    function rotate() {
         undraw();
-        
+        rot = (rot + 1) % 4;
+
         const isAtLeftEdge = rotation.some(index => (currentpos + index) % width === 0);
         const isAtRightEdge = rotation.some(index => (currentpos + index) % width === width - 1);
-        if(!isAtLeftEdge  && !isAtRightEdge)
-        {
-            rot = (rot+1)%4;
-        }
-
-        rotation = tetraminos[tmno][rot];
-        if(rotation.some(index => squares[currentpos + index].classList.contains('taken')))
-        {
-            rot = (rot+3)%4;
+        
+        if(!isAtLeftEdge && !isAtRightEdge){
             rotation = tetraminos[tmno][rot];
+        }  
+        else{
+            rot = (rot + 3) % 4;
         }
-        draw();
+        
     }
+    
 
     function isCollision(position) {
         return rotation.some(index =>
-            squares[position + index].classList.contains('taken') 
+            squares[position + index].classList.contains('taken')
         );
     }
-    
+
 
     function freez() {
         if (rotation.some(index =>
-            squares[currentpos + index + width].classList.contains('taken') 
+            squares[currentpos + index + width].classList.contains('taken')
         )) {
+            
             rotation.forEach(index => {
                 squares[currentpos + index].classList.add('taken');
             })
-    
+
             currentpos = random_pos();
-            rotation = random_rotation();
+            [tmno,rot] = [nexttmno,nextrot];
+            rotation = tetraminos[tmno][rot];
             draw();
+            undraw_next();
+            let [newnexttmno,newnextrot] = random_rotation();
+            nextrotation = nexttmnos[newnexttmno];
+            [nexttmno,nextrot] = [newnexttmno,newnextrot];
+            
+            draw_next();
         }
     }
-    
+
 }) 
